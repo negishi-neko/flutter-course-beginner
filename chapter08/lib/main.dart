269e2980159718e553c95981fed2b95730a2ea64
NepiaaaaN <surface095@gmail.com>
2025-02-11 22:53:31 +0900

    chapter8 inplement-web-view

--- chapter08/lib/main.dart
+++ chapter08/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,20 +30,33 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // 変数が宣言時ではなく使われるタイミングで初期化される
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    // loadRequestの結果ではなく、カスケード演算子前のオブジェクトが返される
+    // 以下と同義
+    // controller = WebViewController();
+    // controller.loadRequest(Uri.parse('https://nzigen.com'));
+    controller = WebViewController()
+      ..loadRequest(Uri.parse('https://www.kuzirawkmgifu.com/'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Image.network(
-          'https://ogre.natalie.mu/artist/106738/20180810/KUZIRA_art201805.jpg?imwidth=400&imdensity=1',
-          width: 400,
-          height: 400,
-        ),
-      ),
-    );
+        body: SafeArea(
+      child: WebViewWidget(controller: controller),
+    )
+        // Center(
+        //   child: Image.network(
+        //     'https://ogre.natalie.mu/artist/106738/20180810/KUZIRA_art201805.jpg?imwidth=400&imdensity=1',
+        //     width: 400,
+        //     height: 400,
+        //   ),
+        // ),
+        );
   }
 }
